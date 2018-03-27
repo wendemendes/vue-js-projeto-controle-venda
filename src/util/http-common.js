4b8fc3b2fc4123bfc394695ed690cc72a37e8715
Wende Mendes <wende@bluesoft.com.br>
2018-03-27 13:26:55 -0300

    refatorando

--- src/util/http-common.js
+++ src/util/http-common.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export const HTTP = axios.create({
   baseURL:  'http://localhost:8088/controle-venda/',
   headers: {
-    Authorization: 'Bearer {token}'
+   'Content-Type': 'application/json',
+    withCredentials: true,
   }
 })
